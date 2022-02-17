92469a7bf42eac72304598cf731362f39db5fbcd
DinossauroBebado <guipsilva32@gmail.com>
2022-02-17 14:30:43 -0300

    how to make changes to the model

--- Init/source/products/models.py
+++ Init/source/products/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+    # blank = if it can our cant be left with out any info in it
+    # null = if its ins empty how the database will safe that information
+
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
     sumary = models.TextField(default='capivara')
+    feature = models.BooleanField()  # null=True, blank=True
